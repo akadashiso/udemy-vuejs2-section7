968aa3d29c163af9f3ca3e62f3792d8ac29955eb
akadashiso <akadashiso@gmail.com>
2021-06-09 17:36:21 +0900

    [Finish]Section7-94 : ネストされたルート

--- vuerouter/src/router/index.js
+++ vuerouter/src/router/index.js
@@ -5,6 +5,9 @@ import BookList from '../views/BookList.vue'
 import BookDetail from '@/components/BookDetail.vue'
 import Item from '../views/Item.vue'
 import NotFound from '@/components/NotFound.vue'
+import User from '../views/User.vue'
+import UserProfile from '@/components/UserProfile.vue'
+import UserPost from '@/components/UserPost.vue'
 
 Vue.use(VueRouter)
 
@@ -46,7 +49,21 @@ const routes = [
     path: '/item/:id',
     name: 'Item',
     component: Item
-  }
+  },
+  {
+    path: '/user',
+    component: User,
+    children: [
+      {
+        path: 'profile',
+        component: UserProfile
+      },
+      {
+        path: 'post',
+        component: UserPost
+      },
+    ],
+  },
 ]
 
 const router = new VueRouter({
